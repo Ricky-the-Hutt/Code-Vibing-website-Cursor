c995ce246ec59852fd1a89ec9c180aaf41a2fc9d
Ricardo <ricardo.lopes@ua.pt>
2026-01-17 18:08:22 +0000

    Finalize header margins: pt-8 top and px-7 side

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,24 +6,24 @@ export default function Header() {
   return (
     <header className="bg-white dark:bg-black">
       <div className="pb-3 dark:pb-3">
-        <nav className="pt-5 pv3 ph2 max-w-7xl mx-auto" role="navigation">
-          <div className="flex justify-between items-center gap-4">
-            <Link href="/" className="text-2xl font-light no-underline text-gray-900 dark:text-gray-300 max-w-lg block leading-tight">
+        <nav className="pt-8 py-4 px-7 container-fluid" role="navigation">
+          <div className="flex justify-between items-center">
+            <Link href="/" className="text-2xl font-light no-underline text-gray-900 dark:text-gray-300 block leading-tight">
               Looking after goodness and truth
             </Link>
             <div className="flex items-center">
-              <ul className="pl-0 mr-3 flex">
-                <li className="list f5 f4-ns fw4 pr-3">
+              <ul className="pl-0 mr-3 flex list-none m-0">
+                <li className="f5 f4-ns fw4 pr-3">
                   <Link href="/blog" className="no-underline text-gray-900 dark:text-gray-300">
                     Blog
                   </Link>
                 </li>
-                <li className="list f5 f4-ns fw4 pr-3">
+                <li className="f5 f4-ns fw4 pr-3">
                   <Link href="/cv" className="no-underline text-gray-900 dark:text-gray-300">
                     CV
                   </Link>
                 </li>
-                <li className="list f5 f4-ns fw4 pr-3">
+                <li className="f5 f4-ns fw4 pr-3">
                   <Link href="/" className="no-underline text-gray-900 dark:text-gray-300">
                     About
                   </Link>
